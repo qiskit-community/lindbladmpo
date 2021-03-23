503e17786bf2fa0115d33681a1bccf6dd851a503
Grégoire Misguich <gregoire.misguich@cea.fr>
2021-03-23 12:41:20 +0100

    Corrected an error (confusion between number of bonds, num_bonds, and number of sites, N)

--- MyLindbladian.h
+++ MyLindbladian.h
@@ -11,6 +11,7 @@ void SetLindbladian(SpinHalfSystem &C, ModelParameters param, Lattice2d L)
     // -----------------------------------------------------------
     // We first construct the Hamiltonian (unitary evolution) terms
     const unsigned int N = C.N;
+    const unsigned int num_bonds=L.I.size();
     vector<double> h_x = param.doublevec("h_x");
     unsigned int h_x_len = h_x.size();
     if (h_x_len != 1 && h_x_len != N)
@@ -20,18 +21,18 @@ void SetLindbladian(SpinHalfSystem &C, ModelParameters param, Lattice2d L)
         h_x = vector<double>(N, h_x[0]);
     }
     vector<double> J = param.doublevec("J");
-    if (J.size() != 1 && J.size() != N)
-        cerr << "Error: the paramter J has " << J.size() << " values  1 or " << N << " value(s) were expected.\n", exit(1);
+    if (J.size() != 1 && J.size() != num_bonds)
+        cerr << "Error: the paramter J has " << J.size() << " values  but  1 or " << num_bonds << " value(s) were expected.\n", exit(1);
     if (J.size() == 1)
     {
-        J = vector<double>(N, J[0]);
+        J = vector<double>(num_bonds, J[0]);
     }
     vector<double> J_z = param.doublevec("J_z");
-    if (J_z.size() != 1 && J_z.size() != N)
-        cerr << "Error: the paramter J has " << J_z.size() << " values  1 or " << N << " value(s) were expected.\n", exit(1);
+    if (J_z.size() != 1 && J_z.size() != num_bonds)
+        cerr << "Error: the paramter J has " << J_z.size() << " values  but  1 or " << num_bonds << " value(s) were expected.\n", exit(1);
     if (J_z.size() == 1)
     {
-        J_z = vector<double>(N, J_z[0]);
+        J_z = vector<double>(num_bonds, J_z[0]);
     }
 
     const double Delta = param.val("Delta");
@@ -44,7 +45,7 @@ void SetLindbladian(SpinHalfSystem &C, ModelParameters param, Lattice2d L)
         //Note about the Lindblad equation: since we evolve rho (and not a wave function), each term in H
         //acts once with a "+" on the right of rho, and once with a "-" to the left of rho (prefix "_" in the operator name)
 
-        for (unsigned int n = 0; n < L.I.size(); n++)
+        for (unsigned int n = 0; n < num_bonds; n++)
         {
             int i = L.I[n], j = L.J[n];
             auto_L += -J[n], "S+", i, "S-", j;
