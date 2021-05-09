a93638bb577293fa910a7b82b9648a44fa039470
Gal Shmulovitz <galvz357@gmail.com>
2021-05-09 17:41:55 +0300

    added comments

--- examples/first_execution.py
+++ examples/first_execution.py
@@ -1,11 +1,14 @@
-from python.MPOLindbladSolver import MPOLindbladSolver
+from . import MPOLindbladSolver
 '''
 sim.build_input_file(dict_val)
 sim.execute("C:/cygwin64/bin/bash.exe --login -i -c",
                       "/cygdrive/c/Users/galvz/AppData/Roaming/SPB_Data/Lindbladian-MPO-simulator",
                       "C:/Users/galvz/PycharmProjects/sim_func/newfile.txt")
 '''
+# crate the parameters dictionary
 dict_val = {'tau': 0.2, 't_final': 2, 'input_file': "C:/Users/galvz/PycharmProjects/sim_func/newfile.txt", 'output_file': "C:/Users/galvz/AppData/Roaming/SPB_Data/Lindbladian-MPO-simulator/out"}
+# initialize class - parameters, cygwin_path, simulator_path
 sim = MPOLindbladSolver(dict_val, "C:/cygwin64/bin/bash.exe --login -i -c",
                         "/cygdrive/c/Users/galvz/AppData/Roaming/SPB_Data/Lindbladian-MPO-simulator")
+# execute simulator
 sim.solve()
