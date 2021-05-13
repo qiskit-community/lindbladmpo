309439b9ce18ca7219f097fa42b12abea5e0a9b7
Haggai Landa <haggai.landa@ibm.com>
2021-05-13 13:28:12 +0300

    Changed command line strings

--- examples/first_execution.py
+++ examples/first_execution.py
@@ -1,14 +1,19 @@
-from . import MPOLindbladSolver
+from python.MPOLindbladSolver import MPOLindbladSolver
+# crate the parameters dictionary
+dict_val = {'tau': 0.2, 't_final': 2,
+            'input_file': "C:/Users/HaggaiLanda/Box/Haggai/python/MPO/newfile.txt",
+            'output_file': "C:/Users/HaggaiLanda/Box/Haggai/python/MPO"}
+# initialize class - parameters, cygwin_path, simulator_path
+sim = MPOLindbladSolver(dict_val, "C:/cygwin64/bin/bash.exe",
+                        "/cygdrive/c/Users/HaggaiLanda/gitprojects/Lindbladian-MPO-simulator/lindblad.exe")
+
+# execute simulator
+sim.solve()
+
+
 '''
 sim.build_input_file(dict_val)
 sim.execute("C:/cygwin64/bin/bash.exe --login -i -c",
                       "/cygdrive/c/Users/galvz/AppData/Roaming/SPB_Data/Lindbladian-MPO-simulator",
                       "C:/Users/galvz/PycharmProjects/sim_func/newfile.txt")
 '''
-# crate the parameters dictionary
-dict_val = {'tau': 0.2, 't_final': 2, 'input_file': "C:/Users/galvz/PycharmProjects/sim_func/newfile.txt", 'output_file': "C:/Users/galvz/AppData/Roaming/SPB_Data/Lindbladian-MPO-simulator/out"}
-# initialize class - parameters, cygwin_path, simulator_path
-sim = MPOLindbladSolver(dict_val, "C:/cygwin64/bin/bash.exe --login -i -c",
-                        "/cygdrive/c/Users/galvz/AppData/Roaming/SPB_Data/Lindbladian-MPO-simulator")
-# execute simulator
-sim.solve()
