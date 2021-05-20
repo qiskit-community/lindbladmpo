447e8810ce7a5a7e368df96fbffa3ff378d2b619
Gal Shmulovitz <galvz357@gmail.com>
2021-05-20 19:36:38 +0300

    remove redundant parentheses

--- python/MPOLindbladSolver.py
+++ python/MPOLindbladSolver.py
@@ -73,35 +73,35 @@ class MPOLindbladSolver:
 
 	@staticmethod
 	def is_float(value):
-		return (isinstance(value, float) or isinstance(value, int))
+		return isinstance(value, float) or isinstance(value, int)
 
 	@staticmethod
 	def is_a_float_list(lst):
-		if (not isinstance(lst, list)):
+		if not isinstance(lst, list):
 			return False
 		for element in lst:
-			if (not MPOLindbladSolver.is_float(element)):
+			if not MPOLindbladSolver.is_float(element):
 				return False
 		return True
 
 	@staticmethod
 	def get_number_of_qubits(dict_in):
-		if (("l_x" in dict_in) and ("l_y" in dict_in)):
-			if (MPOLindbladSolver.is_int(dict_in["l_x"]) and MPOLindbladSolver.is_int(dict_in["l_y"])):
-				return (dict_in["l_x"] * dict_in["l_y"])
-		elif ("n_qubits" in dict_in):
-			if (MPOLindbladSolver.is_int(dict_in["n_qubits"])):
+		if ("l_x" in dict_in) and ("l_y" in dict_in):
+			if MPOLindbladSolver.is_int(dict_in["l_x"]) and MPOLindbladSolver.is_int(dict_in["l_y"]):
+				return dict_in["l_x"] * dict_in["l_y"]
+		elif "n_qubits" in dict_in:
+			if MPOLindbladSolver.is_int(dict_in["n_qubits"]):
 				return dict_in["n_qubits"]
 		return -1
 
 	@staticmethod
 	def check_argument_correctness(dict_in):
 		"""
-		Returns a detailed Error message if dict_in agruments are not in the correct format.
+		Returns a detailed Error message if dict_in arguments are not in the correct format.
 			Args:
-				dict_in: a dictionary with the parameters for the simulaton, keys as agruments and values as values.
+				dict_in: a dictionary with the parameters for the simulation, keys as arguments and values as values.
 			Returns:
-				A detailed Error message if dict_in agruments are not in the correct format (which is stated in the spec of the simulator).
+				A detailed Error message if dict_in arguments are not in the correct format (which is stated in the spec of the simulator).
 				else, returns "" (checks passed).
 			Raises:
 				Nothing, all issues will come out in the output returned.
@@ -110,177 +110,177 @@ class MPOLindbladSolver:
 		check_msg = ""
 		for key in dict.keys(dict_in):
 
-			if (key == "n_qubits"):
-				if (not MPOLindbladSolver.is_int(dict_in[key])):
+			if key == "n_qubits":
+				if not MPOLindbladSolver.is_int(dict_in[key]):
 					check_msg += "Error: " + key + " should be an integer\n"
 				else:
-					if (dict_in[key] < 1):
+					if dict_in[key] < 1:
 						check_msg += "Error: " + key + " should be bigger/equal to 1 (integer)\n"
 
-			elif ((key == "t_final") or (key == "output_step")):
-				if (not MPOLindbladSolver.is_int(dict_in[key])):
+			elif (key == "t_final") or (key == "output_step"):
+				if not MPOLindbladSolver.is_int(dict_in[key]):
 					check_msg += "Error: " + key + " should be an integer\n"
 
-			elif (key == "tau"):
-				if (MPOLindbladSolver.is_float(dict_in[key])):
-					if (dict_in[key] <= 0):
+			elif key == "tau":
+				if MPOLindbladSolver.is_float(dict_in[key]):
+					if dict_in[key] <= 0:
 						check_msg += "Error: " + key + " must be bigger then 0\n"
 				else:
 					check_msg += "Error: " + key + " is not a float\n"
 
 			elif ((key == "h_x") or (key == "h_y") or (key == "h_z") or (key == "g_0") or (key == "g_1") or (
 					key == "g_2")):
-				if (not MPOLindbladSolver.is_float(dict_in[key])):
-					if (isinstance(dict_in[key], list)):
+				if not MPOLindbladSolver.is_float(dict_in[key]):
+					if isinstance(dict_in[key], list):
 						number_of_qubits = MPOLindbladSolver.get_number_of_qubits(dict_in)
-						if (number_of_qubits == -1):
-							check_msg += "Error: " + key + " could not be validated because 'n_qubits' (or alternativly l_x, l_y) are not defined properly\n"
+						if number_of_qubits == -1:
+							check_msg += "Error: " + key + " could not be validated because 'n_qubits' (or alternatively l_x, l_y) are not defined properly\n"
 						else:
-							if (len(dict_in[key]) != number_of_qubits):
+							if len(dict_in[key]) != number_of_qubits:
 								check_msg += "Error: " + key + " is not a float or a N_Qubits size list (of floats)\n"
 							else:
 								for element in dict_in[key]:
-									if (not MPOLindbladSolver.is_float(element)):
+									if not MPOLindbladSolver.is_float(element):
 										check_msg += "Error: " + key + " is not a float or a N_Qubits size list (of floats)\n"
 										break
 					else:
 						check_msg += "Error: " + key + " is not a float or a N_Qubits size list (of floats)\n"
 
-			elif ((key == "J_z") or (key == "J")):
-				if ((dict_in[key] != "") and (not MPOLindbladSolver.is_float(dict_in[key]))):
-					if (not isinstance(dict_in[key], list)):
+			elif (key == "J_z") or (key == "J"):
+				if (dict_in[key] != "") and (not MPOLindbladSolver.is_float(dict_in[key])):
+					if not isinstance(dict_in[key], list):
 						check_msg += "Error: " + key + " should be either empty/const/square matrix (floats)\n"
 					else:
 						number_of_qubits = MPOLindbladSolver.get_number_of_qubits(dict_in)
-						if (number_of_qubits == -1):
-							check_msg += "Error: " + key + " could not be validated because 'n_qubits' (or alternativly l_x, l_y) are not defined properly\n"
+						if number_of_qubits == -1:
+							check_msg += "Error: " + key + " could not be validated because 'n_qubits' (or alternatively l_x, l_y) are not defined properly\n"
 						else:
-							if (len(dict_in[key]) != number_of_qubits):
+							if len(dict_in[key]) != number_of_qubits:
 								check_msg += "Error: " + key + " should be either empty/const/square matrix in the size of qubits^2 (floats)\n"
 							else:
 								for lst in dict_in[key]:
-									if (not isinstance(lst, list)):
+									if not isinstance(lst, list):
 										check_msg += "Error: " + key + " should be either empty/const/square matrix (floats)\n"
 										break
-									elif (len(lst) != number_of_qubits):
+									elif len(lst) != number_of_qubits:
 										check_msg += "Error: " + key + " should be either empty/const/square matrix in the size of qubits^2 (floats)\n"
 										break
 									else:
 										for val in lst:
-											if (not MPOLindbladSolver.is_float(val)):
+											if not MPOLindbladSolver.is_float(val):
 												check_msg += "Error: " + key + " should be either empty/const/square matrix (floats)\n"
 												break
 
-			elif (key == "init_pure_state"):
-				if (not isinstance(dict_in[key], str)):
+			elif key == "init_pure_state":
+				if not isinstance(dict_in[key], str):
 					check_msg += "Error: " + key + " is not a string\n"
 				else:
-					if (len(dict_in[key]) != 2):
+					if len(dict_in[key]) != 2:
 						check_msg += "Error: " + key + " is a string but not of length 2 !\n"
 
 			elif ((key == "b_periodic_x") or (key == "b_periodic_y") or (key == "b_force_rho_trace") or (
 					key == "b_force_rho_hermitian")):
-				if (not isinstance(dict_in[key], bool)):
-					check_msg += "Error: " + key + " should be a boolian True or False as its a switch\n"
+				if not isinstance(dict_in[key], bool):
+					check_msg += "Error: " + key + " should be a boolean True or False as its a switch\n"
 
-			elif (key == "trotter_order"):
+			elif key == "trotter_order":
 				if ((not MPOLindbladSolver.is_int(dict_in[key])) and dict_in[key] != 2 and dict_in[key] != 3 and
 						dict_in[key] != 4):
 					check_msg += "Error: " + key + " should be 2,3 or 4\n"
 
-			elif ((key == "max_dim") or (key == "max_dim_rho")):  # int
-				if (not MPOLindbladSolver.is_int(dict_in[key])):
+			elif (key == "max_dim") or (key == "max_dim_rho"):  # int
+				if not MPOLindbladSolver.is_int(dict_in[key]):
 					check_msg += "Error: " + key + " should be an integer\n"
 
-			elif ((key == "cut_off") or (key == "cut_off_rho")):
-				if (not MPOLindbladSolver.is_float(dict_in[key])):
+			elif (key == "cut_off") or (key == "cut_off_rho"):
+				if not MPOLindbladSolver.is_float(dict_in[key]):
 					check_msg += "Error: " + key + " is not a small float format (ae-b where a and b are numbers)\n"
 
-			elif (key == "save_state_file"):
+			elif key == "save_state_file":
 				pass  # fixme: add a check for windows/linux/mac path string validation
-			elif (key == "output_file"):
+			elif key == "output_file":
 				pass
-			elif (key == "input_file"):
+			elif key == "input_file":
 				pass
-			elif (key == "1q_components"):
-				if (dict_in[key] != ""):
+			elif key == "1q_components":
+				if dict_in[key] != "":
 					x_c = 0
 					y_c = 0
 					z_c = 0
-					if (not isinstance(dict_in[key], list)):
+					if not isinstance(dict_in[key], list):
 						check_msg += "Error: " + key + " should get a of sizes 1,2,3 with x,y,z)\n"
 					else:
-						if (len(dict_in[key]) > 3):
+						if len(dict_in[key]) > 3:
 							check_msg += "Error: " + key + " should get a of sizes 1,2,3 with x,y,z)\n"
 						else:
 							for val in dict_in[key]:
 								val = str.lower(val)
-								if ((val == "x") and (x_c != 1)):
+								if (val == "x") and (x_c != 1):
 									x_c += 1
 
-								elif ((val == "y") and (y_c != 1)):
+								elif (val == "y") and (y_c != 1):
 									y_c += 1
 
-								elif ((val == "z") and (z_c != 1)):
+								elif (val == "z") and (z_c != 1):
 									z_c += 1
 
 								else:
 									check_msg += "Error: " + key + " only gets x,y,z (or a subset)\n"
 									break
 
-			elif (key == "1q_sites"):
-				if (dict_in[key] != ""):
-					if (not isinstance(dict_in[key], list)):
+			elif key == "1q_sites":
+				if dict_in[key] != "":
+					if not isinstance(dict_in[key], list):
 						check_msg += "Error: " + key + " should be an integer list (1,2,3,4..)\n"
 					else:
 						for element in dict_in[key]:
-							if (not MPOLindbladSolver.is_int(element)):
+							if not MPOLindbladSolver.is_int(element):
 								check_msg += "Error: " + key + " should be an integer list (1,2,3,4..)\n"
 								break
 
-			elif (key == "2q_components"):
-				if (dict_in[key] != ""):
+			elif key == "2q_components":
+				if dict_in[key] != "":
 					chkm = [0, 0, 0, 0, 0, 0]
-					if (not isinstance(dict_in[key], list)):
+					if not isinstance(dict_in[key], list):
 						check_msg += "Error: " + key + " only receives xx,yy,zz,xy,xz,yz (or a subset) as a strings list\n"
 					else:
-						if (len(dict_in[key]) > 6):
+						if len(dict_in[key]) > 6:
 							check_msg += "Error: " + key + " only receives xx,yy,zz,xy,xz,yz (or a subset)\n"
 						else:
 							for val in dict_in[key]:
 								val = str.lower(val)
-								if ((val == "xx") and (chkm[0] != 1)):
+								if (val == "xx") and (chkm[0] != 1):
 									chkm[0] += 1
-								elif ((val == "yy") and (chkm[1] != 1)):
+								elif (val == "yy") and (chkm[1] != 1):
 									chkm[1] += 1
-								elif ((val == "zz") and (chkm[2] != 1)):
+								elif (val == "zz") and (chkm[2] != 1):
 									chkm[2] += 1
-								elif (((val == "xy") or (val == "yx")) and (chkm[3] != 1)):
+								elif ((val == "xy") or (val == "yx")) and (chkm[3] != 1):
 									chkm[3] += 1
-								elif (((val == "xz") or (val == "zx")) and (chkm[4] != 1)):
+								elif ((val == "xz") or (val == "zx")) and (chkm[4] != 1):
 									chkm[4] += 1
-								elif (((val == "yz") or (val == "zy")) and (chkm[5] != 1)):
+								elif ((val == "yz") or (val == "zy")) and (chkm[5] != 1):
 									chkm[5] += 1
 								else:
 									check_msg += "Error: " + key + " only receives xx,yy,zz,xy,xz,yz (or a subset)\n"
 									break
 
-			elif (key == "2q_sites"):  # expecting an integer tuples list
-				if (dict_in[key] != ""):
-					if (not isinstance(dict_in[key], list)):
-						check_msg += "Error: " + key + " should be an list of tuples of size 2, containing ingeter\n"
+			elif key == "2q_sites":  # expecting an integer tuples list
+				if dict_in[key] != "":
+					if not isinstance(dict_in[key], list):
+						check_msg += "Error: " + key + " should be an list of tuples of size 2, containing integer\n"
 					else:
 						for tup in dict_in[key]:
-							if (not isinstance(tup, tuple)):
-								check_msg += "Error: " + key + " should be an list of tuples of size 2, containing ingeter\n"
+							if not isinstance(tup, tuple):
+								check_msg += "Error: " + key + " should be an list of tuples of size 2, containing integer\n"
 								break
 							if ((not MPOLindbladSolver.is_int(tup[0])) or (not MPOLindbladSolver.is_int(tup[1])) or (
 									len(tup) != 2)):
-								check_msg += "Error: " + key + " should be an list of tuples of size 2, containing ingeters\n"
+								check_msg += "Error: " + key + " should be an list of tuples of size 2, containing integers\n"
 								break
 
 			else:
-				check_msg += "Error: " + "Error: user inserted invalid key (agrument): " + key + "\n"
+				check_msg += "Error: " + "Error: user inserted invalid key (argument): " + key + "\n"
 		return check_msg
 
 	@staticmethod
@@ -332,9 +332,8 @@ class MPOLindbladSolver:
 
 	@staticmethod
 	def load_output(s_output_file):
-		result = {}
-		result['1q'] = MPOLindbladSolver.read_1q_output_into_dict(s_output_file)
-		result['2q'] = MPOLindbladSolver.read_2q_output_into_dict(s_output_file)
+		result = {'1q': MPOLindbladSolver.read_1q_output_into_dict(s_output_file),
+				  '2q': MPOLindbladSolver.read_2q_output_into_dict(s_output_file)}
 		return result
 
 	def solve(self):
