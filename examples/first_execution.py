de0ef8b8097293e0e8fff1b1a12a62dc97a2a368
Haggai Landa <haggai.landa@ibm.com>
2021-05-24 16:13:05 +0300

    space-time plotting enhancements

--- examples/first_execution.py
+++ examples/first_execution.py
@@ -5,21 +5,26 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 
 # Simulation parameters
-n_qubits = 5
-s_path = "C:/Users/galvz/PycharmProjects/sim_func/"
-s_executable = "/cygdrive/c/Users/galvz/AppData/Roaming/SPB_Data/Lindbladian-MPO-simulator/lindblad.exe"
+n_qubits = 16
+# s_path = "C:/Users/galvz/PycharmProjects/sim_func/"
+# s_executable = "/cygdrive/c/Users/galvz/AppData/Roaming/SPB_Data/Lindbladian-MPO-simulator/lindblad.exe"
+s_path = "C:/temp/"
+s_executable = "/cygdrive/c/Users/HaggaiLanda/gitprojects/Lindbladian-MPO-simulator/lindblad.exe"
+b_save_figures = True
+fontsize = 16
+
 h_x = np.random.randn(n_qubits).tolist()
 h_y = np.random.randn(n_qubits).tolist()
 h_z = np.random.randn(n_qubits).tolist()
 g_1 = (0.1 * np.random.rand(n_qubits)).tolist()
 J = .5
-t_final = 10
-tau = 1
+t_final = 1
+tau = .02
 l_x = 0
 
 # create the parameters dictionary
 dict_val = {'tau': tau, 't_final': t_final, 'N': n_qubits,
-            'h_x': h_x, 'h_x': h_y, 'h_x': h_z, 'g_1': g_1, 'J': J, 'l_x': l_x,
+            'h_x': h_x, 'h_z': h_z, 'g_1': g_1, 'J': J, 'l_x': l_x,
             'input_file': s_path + "MPO.input",
             'output_file': s_path + "MPO"}
 # initialize class - parameters, cygwin_path, simulator_path
@@ -28,22 +33,35 @@ sim = MPOLindbladSolver(dict_val, "C:/cygwin64/bin/bash.exe", s_executable)
 # execute simulator
 sim.solve()
 
-data = np.zeros(shape=(n_qubits, int(t_final/tau) + 1))
+n_t_steps = int(t_final/tau) + 1
+data = np.zeros(shape=(n_qubits, n_t_steps))
 i = 0
 for key in sim.result['1q']:
     if key[1] == 'Z':
         data[key[0] - 1, i] = sim.result['1q'][key]
         if key[0] == n_qubits:
             i += 1
+t_steps = np.arange(0, n_t_steps, int(n_t_steps / 10))
+t_ticks = np.round(t_steps * tau, 5)
+r_qubits = range(n_qubits)
 
-ax = plt.subplot()
-im = ax.imshow(data, interpolation='none')
+fig, ax = plt.subplots(figsize = (14, 8))
+plt.rcParams.update({'font.size': fontsize})
+im = ax.imshow(data, interpolation='none', aspect = 'auto')
 divider = make_axes_locatable(ax)
 cax = divider.append_axes("right", size="5%", pad=0.05)
 plt.colorbar(im, cax=cax)
+ax.set_xlabel('$t$', fontsize = fontsize)
+ax.set_xticks(t_steps)
+ax.set_xticklabels(t_ticks, fontsize = fontsize)
+ax.set_yticks(r_qubits)
+ax.set_yticklabels(r_qubits, fontsize = fontsize)
+ax.set_ylabel('qubits', fontsize = fontsize)
+ax.set_title('$\\langle\sigma_z(t)\\rangle$')
+s_file = f"sigma_z(t), N = {n_qubits}, disordered 1D lattice"
+if b_save_figures:
+    plt.savefig(s_path + s_file)
 
-ax.set_xlabel('time')
-ax.set_ylabel('qubits')
-ax.set_title('Pauli Z expectation value as function of time')
 plt.show()
+tmp = 2
 
