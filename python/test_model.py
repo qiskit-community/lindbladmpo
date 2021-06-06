f54be554c250594a4041de85ec1bb56a033fc2d9
Gal Shmulovitz <galvz357@gmail.com>
2021-06-06 14:55:48 +0300

    added model tests

--- python/test_model.py
+++ python/test_model.py
@@ -7,14 +7,39 @@ s_executable = "/cygdrive/c/Users/galvz/AppData/Roaming/SPB_Data/Lindbladian-MPO
 
 
 class TestModel(unittest.TestCase):
-    def test_something(self):
+    def test_All_zero(self):
         solver_params = {'tau': 1, 't_final': 1, 'N': 1, 'g_1': 0,
                          'input_file': s_path + "MPO.input",
                          'output_file': s_path + "MPO"}
         solver = MPOLindbladSolver(solver_params, "C:/cygwin64/bin/bash.exe", s_executable)
         solver.solve()
         expected_XY = 0
-        expected_Z = 0.5
+        expected_Z = 1
+        self.assertEqual(solver.result['1q'][(1, 'X', 1)], expected_XY)
+        self.assertEqual(solver.result['1q'][(1, 'Y', 1)], expected_XY)
+        self.assertEqual(solver.result['1q'][(1, 'Z', 1)], expected_Z)
+
+
+    def test_hz_not_zero(self):
+        solver_params = {'tau': 1, 't_final': 1, 'N': 1, 'g_1': 0, 'l_x': 0, 'h_z': 5,
+                         'input_file': s_path + "MPO.input",
+                         'output_file': s_path + "MPO"}
+        solver = MPOLindbladSolver(solver_params, "C:/cygwin64/bin/bash.exe", s_executable)
+        solver.solve()
+        expected_XY = 0
+        expected_Z = 1
+        self.assertEqual(solver.result['1q'][(1, 'X', 1)], expected_XY)
+        self.assertEqual(solver.result['1q'][(1, 'Y', 1)], expected_XY)
+        self.assertEqual(solver.result['1q'][(1, 'Z', 1)], expected_Z)
+
+    def test_steady_state(self):
+        solver_params = {'tau': 1, 't_final': 1, 'N': 1, 'g_1': 5, 'l_x': 0, 'h_z': 5,
+                         'input_file': s_path + "MPO.input",
+                         'output_file': s_path + "MPO"}
+        solver = MPOLindbladSolver(solver_params, "C:/cygwin64/bin/bash.exe", s_executable)
+        solver.solve()
+        expected_XY = 0
+        expected_Z = 1
         self.assertEqual(solver.result['1q'][(1, 'X', 1)], expected_XY)
         self.assertEqual(solver.result['1q'][(1, 'Y', 1)], expected_XY)
         self.assertEqual(solver.result['1q'][(1, 'Z', 1)], expected_Z)
