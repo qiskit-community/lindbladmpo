3d848a4683234359a53e27313fc3f3cc71c08788
Haggai Landa <haggai.landa@ibm.com>
2021-07-09 16:39:29 +0300

    Added setup.py, minor fixes

---
+++ setup.py
@@ -0,0 +1,74 @@
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+
+import os
+import re
+import platform
+import subprocess
+
+from setuptools import setup, find_packages, Extension
+
+# with open('requirements.txt') as f:
+# 	REQUIREMENTS = f.read().splitlines()
+REQUIREMENTS = 'itensor=3'
+
+s_system = platform.system().lower()
+if s_system == 'windows':
+	raise Exception('Automatic building of the package is not currently supported on Windows OS.'
+					'Please follow the installation guide in the root directory for building instructions.')
+
+call_string = './src/make'
+process = subprocess.Popen(call_string, shell = True)
+exit_code = process.wait()
+print(f"Make process terminated with exit code {exit_code}")
+
+# Read long description from README.
+README_PATH = os.path.join(os.path.abspath(os.path.dirname(__file__)),
+						   'README.md')
+with open(README_PATH) as readme_file:
+	README = re.sub(
+		'<!--- long-description-skip-begin -->.*<!--- long-description-skip-end -->', '',
+		readme_file.read(), flags=re.S | re.M)
+
+setup(
+	name="lindblad-mpo",
+	version="0.1.0",
+	description="A matrix-product-operators solver for the Lindblad master equation",
+	long_description=README,
+	long_description_content_type='text/markdown',
+	url="https://github.com/haggaila/lindbladmpo",
+	author="Gregoire Misguich",
+	author_email="gregoire.misguich@ipht.fr",
+	license="Apache 2.0",
+	classifiers=[
+		"Environment :: Console",
+		"License :: OSI Approved :: Apache Software License",
+		"Intended Audience :: Developers",
+		"Intended Audience :: Science/Research",
+		# "Operating System :: Microsoft :: Windows",
+		"Operating System :: MacOS",
+		"Operating System :: POSIX :: Linux",
+		"Programming Language :: Python :: 3.6",
+		"Programming Language :: Python :: 3.7",
+		"Programming Language :: Python :: 3.8",
+		"Programming Language :: Python :: 3.9",
+		"Topic :: Scientific/Engineering",
+	],
+	keywords="mps mpo lindblad sdk quantum",
+	packages=find_packages(exclude=['test*']),
+	install_requires=REQUIREMENTS,
+	include_package_data=True,
+	python_requires=">=3.6",
+	extras_require={
+		'visualization': ['matplotlib>=2.1'],
+	},
+	project_urls={
+		"Source Code": "https://github.com/haggaila/lindbladmpo",
+	},
+	zip_safe=False
+)
--- test/test_model.py
+++ test/test_model.py
@@ -6,7 +6,7 @@
 # copyright notice, and modified files need to carry a notice indicating
 # that they have been altered from the originals.
 
-from lindbladmpo.MPOLindbladSolver import MPOLindbladSolver
+from lindbladmpo.LindbladMPOSolver import LindbladMPOSolver
 import unittest
 import numpy as np
 
@@ -15,55 +15,55 @@ s_executable = "/cygdrive/c/Users/galvz/AppData/Roaming/SPB_Data/Lindbladian-MPO
 
 
 class TestModel(unittest.TestCase):
-    def test_All_zero(self):
-        solver_params = {'tau': 1, 't_final': 1, 'N': 1, 'g_1': 0,
-                         'input_file_prefix': s_path + "MPO.input",
-                         'output_file_prefix': s_path + "MPO"}
-        solver = MPOLindbladSolver(solver_params, "C:/cygwin64/bin/bash.exe", s_executable)
-        solver.solve()
-        expected_XY = 0
-        expected_Z = 1
-        self.assertEqual(solver.result['1q'][(1, 'X', 1)], expected_XY)
-        self.assertEqual(solver.result['1q'][(1, 'Y', 1)], expected_XY)
-        self.assertEqual(solver.result['1q'][(1, 'Z', 1)], expected_Z)
+	def test_All_zero(self):
+		solver_params = {'tau': 1, 't_final': 1, 'N': 1, 'g_1': 0,
+						 'input_file_prefix': s_path + "MPO.input",
+						 'output_file_prefix': s_path + "MPO"}
+		solver = LindbladMPOSolver(solver_params, "C:/cygwin64/bin/bash.exe", s_executable)
+		solver.solve()
+		expected_XY = 0
+		expected_Z = 1
+		self.assertEqual(solver.result['1q'][(1, 'X', 1)], expected_XY)
+		self.assertEqual(solver.result['1q'][(1, 'Y', 1)], expected_XY)
+		self.assertEqual(solver.result['1q'][(1, 'Z', 1)], expected_Z)
 
 
-    def test_hz_not_zero(self):
-        solver_params = {'tau': 1, 't_final': 1, 'N': 1, 'g_1': 0, 'l_x': 0, 'h_z': 5,
-                         'input_file_prefix': s_path + "MPO.input",
-                         'output_file_prefix': s_path + "MPO"}
-        solver = MPOLindbladSolver(solver_params, "C:/cygwin64/bin/bash.exe", s_executable)
-        solver.solve()
-        expected_XY = 0
-        expected_Z = 1
-        self.assertEqual(solver.result['1q'][(1, 'X', 1)], expected_XY)
-        self.assertEqual(solver.result['1q'][(1, 'Y', 1)], expected_XY)
-        self.assertEqual(solver.result['1q'][(1, 'Z', 1)], expected_Z)
+	def test_hz_not_zero(self):
+		solver_params = {'tau': 1, 't_final': 1, 'N': 1, 'g_1': 0, 'l_x': 0, 'h_z': 5,
+						 'input_file_prefix': s_path + "MPO.input",
+						 'output_file_prefix': s_path + "MPO"}
+		solver = LindbladMPOSolver(solver_params, "C:/cygwin64/bin/bash.exe", s_executable)
+		solver.solve()
+		expected_XY = 0
+		expected_Z = 1
+		self.assertEqual(solver.result['1q'][(1, 'X', 1)], expected_XY)
+		self.assertEqual(solver.result['1q'][(1, 'Y', 1)], expected_XY)
+		self.assertEqual(solver.result['1q'][(1, 'Z', 1)], expected_Z)
 
-    def test_steady_state(self):
-        solver_params = {'tau': 1, 't_final': 1, 'N': 1, 'g_1': 5, 'l_x': 0, 'h_z': 5,
-                         'input_file_prefix': s_path + "MPO.input",
-                         'output_file_prefix': s_path + "MPO"}
-        solver = MPOLindbladSolver(solver_params, "C:/cygwin64/bin/bash.exe", s_executable)
-        solver.solve()
-        expected_XY = 0
-        expected_Z = 1
-        self.assertEqual(solver.result['1q'][(1, 'X', 1)], expected_XY)
-        self.assertEqual(solver.result['1q'][(1, 'Y', 1)], expected_XY)
-        self.assertEqual(solver.result['1q'][(1, 'Z', 1)], expected_Z)
+	def test_steady_state(self):
+		solver_params = {'tau': 1, 't_final': 1, 'N': 1, 'g_1': 5, 'l_x': 0, 'h_z': 5,
+						 'input_file_prefix': s_path + "MPO.input",
+						 'output_file_prefix': s_path + "MPO"}
+		solver = LindbladMPOSolver(solver_params, "C:/cygwin64/bin/bash.exe", s_executable)
+		solver.solve()
+		expected_XY = 0
+		expected_Z = 1
+		self.assertEqual(solver.result['1q'][(1, 'X', 1)], expected_XY)
+		self.assertEqual(solver.result['1q'][(1, 'Y', 1)], expected_XY)
+		self.assertEqual(solver.result['1q'][(1, 'Z', 1)], expected_Z)
 
-    def test_steady_state_2(self):
-        solver_params = {'tau': 1, 't_final': 10, 'N': 1, 'g_1': 5, 'g_0': 1, 'l_x': 0, 'h_z': 5,
-                         'input_file_prefix': s_path + "MPO.input",
-                         'output_file_prefix': s_path + "MPO"}
-        solver = MPOLindbladSolver(solver_params, "C:/cygwin64/bin/bash.exe", s_executable)
-        solver.solve()
-        expected_XY = 0
-        expected_Z = 4/6
-        self.assertEqual(solver.result['1q'][(1, 'X', 1)], expected_XY)
-        self.assertEqual(solver.result['1q'][(1, 'Y', 1)], expected_XY)
-        self.assertEqual(solver.result['1q'][(1, 'Z', 1)], expected_Z)
+	def test_steady_state_2(self):
+		solver_params = {'tau': 1, 't_final': 10, 'N': 1, 'g_1': 5, 'g_0': 1, 'l_x': 0, 'h_z': 5,
+						 'input_file_prefix': s_path + "MPO.input",
+						 'output_file_prefix': s_path + "MPO"}
+		solver = LindbladMPOSolver(solver_params, "C:/cygwin64/bin/bash.exe", s_executable)
+		solver.solve()
+		expected_XY = 0
+		expected_Z = 4/6
+		self.assertEqual(solver.result['1q'][(1, 'X', 1)], expected_XY)
+		self.assertEqual(solver.result['1q'][(1, 'Y', 1)], expected_XY)
+		self.assertEqual(solver.result['1q'][(1, 'Z', 1)], expected_Z)
 
 
 if __name__ == '__main__':
-    unittest.main()
+	unittest.main()
