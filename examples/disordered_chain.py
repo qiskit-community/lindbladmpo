efed523c52bd3684d874246de0e19d0206e40a6b
Haggai Landa <haggai.landa@ibm.com>
2021-07-09 13:09:47 +0300

    Makefile edits, examples edit, further minor changes

---
+++ examples/disordered_chain.py
@@ -0,0 +1,64 @@
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+
+import platform
+
+from lindbladmpo.LindbladMPOSolver import *
+from lindbladmpo.solver_routines import *
+
+s_solver_path = os.path.dirname(os.path.abspath(__file__))
+s_system = platform.system().lower()
+if s_system == 'windows':
+	# On Windows we execute the solver using the cygwin bash. Change the following variable if necessary.
+	s_cygwin_path = "C:/cygwin64/bin/bash.exe"
+
+	# s_solver_path should be of the form "/cygdrive/c/ ... ", and we use below a path relative
+	# to the current file's path in the package
+	s_solver_path = s_solver_path.replace(':', '')
+	s_solver_path = s_solver_path.replace('\\', '/')
+	s_solver_path = "/cygdrive/" + s_solver_path
+else:
+	s_cygwin_path = ""
+s_solver_path += "/../bin/lindbladmpo.exe"
+
+s_path = "./output/"
+if not os.path.exists(s_path):
+	os.mkdir(s_path)
+
+# Simulation parameters
+n_qubits = 8
+b_plaquette = True
+b_save_figures = True
+fontsize = 22
+
+h_x = 0. * np.random.randn(n_qubits)
+h_x[int(n_qubits / 2)] = 5.
+h_y = 0. * np.random.randn(n_qubits)
+
+h_z = 0. * np.random.randn(n_qubits)
+g_1 = (.1 * np.random.rand(n_qubits)).tolist()
+J = 1
+t_final = 1
+tau = .05
+
+# Create the parameters dictionary
+s_file_prefix = f"Disordered chain, N = {n_qubits}"
+
+solver_params = {'tau': tau, 't_final': t_final, 'max_dim_rho': 100, 'N': n_qubits, 'b_unique_id': False,
+				 'h_x': h_x, 'h_z': h_z, 'g_1': g_1, 'J': J, 'l_x': n_qubits, 'l_y': 1,
+				 'init_Pauli_state': '+x',
+				 'input_file_prefix': s_path, 'output_file_prefix': s_path + s_file_prefix}
+# Initialize class arguments - the parameters, cygwin path, and MPO executable path
+solver = LindbladMPOSolver(solver_params, s_cygwin_path, s_solver_path)
+# Execute simulator
+solver.solve()
+# Prepare plot data
+data, t_steps, t_ticks, qubits = prepare_plot_data(solver)
+# And plot one figure. By default it will be saved to a file.
+plot_space_time(data, t_steps, t_ticks, qubits, s_path + s_file_prefix)
+plt.show()
---
+++ lindbladmpo/solver_routines.py
@@ -0,0 +1,47 @@
+
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+import matplotlib as plt
+import numpy as np
+from lindbladmpo.LindbladMPOSolver import LindbladMPOSolver
+
+
+def prepare_plot_data(solver: LindbladMPOSolver) -> (np.ndarray, np.ndarray, np.ndarray, np.ndarray):
+	tau = solver.parameters['tau']
+	n_qubits = solver.parameters['N']
+	t_final = solver.parameters['t_final']
+	n_t_steps = int(t_final / tau) + 1
+	data = np.zeros(shape = (n_qubits, n_t_steps), dtype = float)
+	i = 0
+	for key in solver.result['1q']:
+		if key[1] == 'Z':
+			data[key[0] - 1, i] = solver.result['1q'][key]
+			if key[0] == n_qubits:
+				i += 1
+	t_steps = np.arange(0, n_t_steps, int(n_t_steps / 10))
+	t_ticks = np.round(t_steps * tau, 5)
+	qubits = np.asarray(range(n_qubits))
+	return data, t_steps, t_ticks, qubits
+
+
+def plot_space_time(data: np.ndarray, t_steps: np.ndarray, t_ticks: np.ndarray, qubits: np.ndarray,
+					ax = None, fontsize = 16, b_save_figures = True, s_file_prefix = ''):
+	if ax is None:
+		_, ax = plt.subplots(figsize = (14, 9))
+	plt.rcParams.update({'font.size': fontsize})
+	im = ax.imshow(data, interpolation = 'none', aspect = 'auto')
+	divider = make_axes_locatable(ax)
+	cax = divider.append_axes('right', size = '5%', pad = 0.05)
+	plt.colorbar(im, cax = cax)
+	# plt.colorbar(im)
+	ax.set_xlabel('$t$', fontsize = fontsize)
+	ax.set_xticks(t_steps)
+	ax.set_xticklabels(t_ticks, fontsize = fontsize)
+	ax.set_yticks(qubits)
+	ax.set_yticklabels(qubits, fontsize = fontsize)
+	ax.set_ylabel('qubits', fontsize = fontsize)
+	ax.set_title('$\\langle\sigma_z(t)\\rangle$')
+	if b_save_figures:
+		if s_file_prefix != '':
+			s_file_prefix += ', '
+		plt.savefig(s_file_prefix + 'sigma_z(t)')
+
