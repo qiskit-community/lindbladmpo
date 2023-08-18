c52cba4d45b384b6304a1067c543c51fb648647f
robotAstray <robotastray@cantab.net>
2023-08-18 17:35:06 +0100

    Add comprehensive test suite for `find_index_nearest_time_within_tolerance()` (#3)
    * `plot_routines.py`: return index of the closest entry in `find_index_nearest_time_within_tolerance()`
    
    Signed-off-by: robotAstray <robotastray@cantab.net>
    
    * fix docstring in plot_routines.py for `find_index_nearest_time_within_tolerance()`
    
    Signed-off-by: robotAstray <robotastray@cantab.net>
    
    * fix docstring in  plot_routines.py for `find_index_nearest_time_within_tolerance()`
    
    Signed-off-by: robotAstray <robotastray@cantab.net>
    
    * Create `/test/plot/__init__.py`
    
    Signed-off-by: robotAstray <robotastray@cantab.net>
    
    * Create test_plot_routines.py to test `find_index_nearest_time_within_tolerance()`
    
    Signed-off-by: robotAstray <robotastray@cantab.net>
    
    ---------
    
    Signed-off-by: robotAstray <robotastray@cantab.net>

--- lindbladmpo/plot_routines.py
+++ lindbladmpo/plot_routines.py
@@ -54,15 +54,16 @@ def find_index_nearest_time_within_tolerance(arr, target_time, rtol):
         rtol (float): The relative tolerance for considering time values as close.
 
     Returns:
-        int or None: The index of the closest time entry in the array, or None if no
-        sufficiently close time entry was found within the specified tolerance.
+        int: The index of the closest time entry in the array. If no time entry is found
+        that meets the rtol criteria, the function still returns the index of
+        the time entry closest to the target time.
     """
     diff = np.abs(np.array(arr) - target_time)
     closest_index = np.argmin(diff)
     if np.isclose(diff[closest_index], 0, rtol=rtol, atol=0):
         return closest_index
     else:
-        return None
+        return closest_index
 
 
 def prepare_time_data(
