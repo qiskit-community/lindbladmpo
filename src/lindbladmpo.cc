914929f67f72278b83e9ce5a3515ad4c9cdf3b7a
Haggai Landa <55192441+haggaila@users.noreply.github.com>
2024-05-15 17:23:00 +0300

    Updated solver version
    Signed-off-by: Haggai Landa <55192441+haggaila@users.noreply.github.com>

--- src/lindbladmpo.cc
+++ src/lindbladmpo.cc
@@ -28,7 +28,7 @@ using namespace std;
 using namespace std::chrono;
 
 stream2d cout2 = stream2d(&cerr, NULL);
-const string SOLVER_VERSION = "0.2.3";
+const string SOLVER_VERSION = "0.2.4";
 
 const double IMAGINARY_THRESHOLD = 1e-4;
 // Threshold for the imaginary value of a quantity that should be real, to issue a warning
